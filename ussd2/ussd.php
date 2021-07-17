54076f094bfe058da256a3a8f7e769f772e4ba38
kaweesi123'git add* <fahimtannz@gmail.com>
2021-07-17 20:59:50 +0300

    Resolving the witch on the USSD

--- ussd2/ussd.php
+++ ussd2/ussd.php
@@ -1,70 +1,168 @@
-<?php
+ <?php
 header("Content-type:text/plane");
 
-$phone_number - $_POST['phoneNumber'];
+require('../AfricasTalkingGateway.php');
 
-$textFromUser - $_POST['text'];
+require('sql_connection.php');
+
+$phone_number = $_POST['phoneNumber'];
+
+$textFromUser = $_POST['text'];
 
 $sessionID = $_POST['sessionId'];
 
-$serviceCode - $_POST['serviceCode'];
+$serviceCode = $_POST['serviceCode'];
 
 if(empty($textFromUser)){
 
 	$textFromUser = "0";
 
 }else{
-	$textFromUser - "0*".$textFromUser;
+
+	$textFromUser = "0*".$textFromUser;
 
 }
 
 $inputArray = explode("*", $textFromUser);
 
-$level - count($inputArray);
+$level = count($inputArray);
 
-switch ($level){
+switch ($level) {
 
 	case 1:
 
-	$response - "CON Welcome to the Climate (U) Limited";
+		$response = "CON Welcome to the NEMA (U) Limited";
 
-	$response .= "\n 1. Register";
+	    $response .= "\n 1. Register";
 
-	$response .= "\n 2. Add a tree";
+	    $response .= "\n 2. Add a tree";
 
-    echo $response;
+	    echo $response;
 
-    break;
+		break;
 
-    case 2:
+	case 2:		// text = 0*1  OR  0*2 
 
-    if($inputArray[1]) -- 1) {
+		 if($inputArray[1]   ==  1) {//wants to register// 
 
-	echo "CON What is your name?";
+		 	echo "CON What is is your name?";
 
 
-}elseif ($inputArray[1] -- 2) {
 
-}else{
-	echo "END Invaild option";
+		 }elseif ($inputArray[1] == 2) {//wants to add a tree
 
+		 	$checkmembers = $sqliCon->query("SELECT * FROM members WHERE phone_number = '$phone_number' ");
+
+		 	if($checkmembers->num_rows == 0) 
+
+		 		echo "END User with phone_number $phone_number has no account";
+
+		 	else{
+
+		 		while ($results = $checkmembers->fetch_assoc()) {
+
+		 			echo "CON ".$results['name']."\n Enter the number of trees";
+
+		 		}
+
+		 	}
+				
+					  
+
+		 
+		 }else{
+
+		 	echo "END Invalid option";
+
+		 }
+		  
+		break;
+
+	case 3: 
+
+	     if($inputArray[1]   ==  1) {//he wants to register// // 0*1*
+
+		 	$user_name = $inputArray[2];
+
+		 	$saveUser = $sqliCon->query("INSERT INTO members(phone_number,name)VALUES('$phone_number','$user_name')");
+
+		 	if($saveUser){
+
+		 		$message = "Hello ".$user_name." Thank you for registering with NEMA (U) ltd";		        
+				$apikey     = "";			 
+				$gateway    = new AfricasTalkingGateway("sandbox", $apikey,"sandbox");
+
+				$gateway->sendMessage($phone_number, $message); 
+
+				echo "END Thank you for registering"; 
+
+		 	}else{
+
+		 		echo "END Failed to register ".$sqliCon->error;
+
+		 	}
+
+
+		 }elseif ($inputArray[1] == 2) {//he wants to add a tree// // 0*2*7837
+
+		 	$number_of_trees = $inputArray[2];
+
+		 	$checkmembers = $sqliCon->query("SELECT id,name FROM members WHERE phone_number = '$phone_number'");
+
+		 	if($checkmembers->num_rows == 1){
+		 
+		 	while ($rows = $checkmembers->fetch_assoc()) {
+
+		 		$member_id = $rows['id'];
+
+		 	    $member_name = $rows['name'];
+
+		 	    $sqliCon->query("INSERT INTO trees(member_id,number_of_trees)VALUES('$member_id','$number_of_trees')");
+
+		 	    $message = "Hello $member_name, Thank you for conserving the Forests of Uganda. You have recorded $number_of_trees tree(s)";
+			    $apikey     = "";			 
+			    $gateway    = new AfricasTalkingGateway("sandbox", $apikey,"sandbox");
+			    $gateway->sendMessage($phone_number, $message);
+		 	    echo "END $message";
+		 		 
+		 	}
+
+		 }else{
+
+		 	echo "END No user found";
+
+		 }
+	 
+	}  
+
+	 
+		break;
+	
+	default:
+
+		echo"The option selected is not valid";
+
+		break;
 }
 
-break;
-
-case 3:
-   
 
 
-   break;
 
-   default:
+// Welcome message 
 
-   echo"The option selected is not valid";
+// Choose an option 
 
-   break;
+//   1. Register 
+//   2. Add a tree 
+ 
 
-}
+// 1.  WHat is your name?
+//     send them sms that they have registered 
+
+// 2.  Check id they have registered 
+//     Enter number of of trees 
+//      Send a thank you message
+
 
 
 
