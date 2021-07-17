32367775393cb2720c61ecd26d6624fa69cba613
kaweesi123'git add* <fahimtannz@gmail.com>
2021-07-17 14:10:55 +0300

    Working with USSD

---
+++ ussd2/ussd.php
@@ -0,0 +1,12 @@
+<?php
+header("Content-type:text/plane");
+
+$phone_number - $_POST['phoneNumber'];
+
+$textFromUser - $_POST['text'];
+
+$sessionID = $_POST['sessionId'];
+
+$serviceCode - $_POST['serviceCode'];
+
+echo "CON Welcome to the Climate portal";
