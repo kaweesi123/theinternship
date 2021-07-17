b96aee3bba7b5cec3f27987aefc793487238e22f
kaweesi123'git add* <fahimtannz@gmail.com>
2021-07-17 16:55:14 +0300

    Resolving the witch on the USSD

--- ussd2/ussd.php
+++ ussd2/ussd.php
@@ -9,4 +9,63 @@ $sessionID = $_POST['sessionId'];
 
 $serviceCode - $_POST['serviceCode'];
 
-echo "END Welcome to the Climate portal";
+if(empty($textFromUser)){
+
+	$textFromUser = "0";
+
+}else{
+	$textFromUser - "0*".$textFromUser;
+
+}
+
+$inputArray = explode("*", $textFromUser);
+
+$level - count($inputArray);
+
+switch ($level){
+
+	case 1:
+
+	$response - "CON Welcome to the Climate (U) Limited";
+
+	$response .= "\n 1. Register";
+
+	$response .= "\n 2. Add a tree";
+
+    echo $response;
+
+    break;
+
+    case 2:
+
+    if($inputArray[1]) -- 1) {
+
+	echo "CON What is your name?";
+
+
+}elseif ($inputArray[1] -- 2) {
+
+}else{
+	echo "END Invaild option";
+
+}
+
+break;
+
+case 3:
+   
+
+
+   break;
+
+   default:
+
+   echo"The option selected is not valid";
+
+   break;
+
+}
+
+
+
+
